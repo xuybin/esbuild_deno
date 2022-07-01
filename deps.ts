7ee29369730353d64cfd179a74b24d7de258eb11
xuybin <xuybin@qq.com>
2022-07-01 16:33:30 +0800

    refactor:

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
 import type * as esbuild from "./native/mod.d.ts";
 export type { esbuild };
-export { basename, extname, fromFileUrl, resolve, toFileUrl } from "https://deno.land/std@0.142.0/path/mod.ts";
+export { basename, extname, fromFileUrl, resolve, toFileUrl } from "https://deno.land/std@0.145.0/path/mod.ts";
 export { resolveImportMap, resolveModuleSpecifier } from "https://deno.land/x/importmap@0.2.1/mod.ts";
 export type { ImportMap } from "https://deno.land/x/importmap@0.2.1/mod.ts";
